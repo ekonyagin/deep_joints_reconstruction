012dc73dd15f3d72a242f95e22d821efc598af10
egorkonyagin <egorkonyagin@yandex.ru>
2021-01-23 23:46:50 +0300

    created multichannel model

--- joints_conv_model.py
+++ joints_conv_model.py
@@ -5,13 +5,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class PointSolver_conv(nn.Module):
-    def __init__(self, constant,in_channels=1, out_channels=1, kernel_size=3):
+    def __init__(self, constant=None,in_channels=2, out_channels=2, kernel_size=3):
         super(PointSolver_conv, self).__init__()
-        self.constant = constant
+        #self.constant = constant
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.padding = int(np.floor(self.kernel_size/2.))
+        #print(self.padding)
         self.conv11 = nn.Conv1d(in_channels=self.in_channels, 
                                out_channels=16, 
                                kernel_size=self.kernel_size, 
@@ -71,26 +72,27 @@ class PointSolver_conv(nn.Module):
         
         
     def forward(self, x):
-        y = F.relu(self.conv11(self.constant+x))
+        
+        y = F.relu(self.conv11(x))
         y = F.relu(self.conv12(y))
-        y = F.relu(self.conv13(y)) 
+        y = F.relu(self.conv13(y))
         
         y = self.bn1(y)
-        
-        y = F.relu(self.conv21(y+x))
+
+        y = F.relu(self.conv21(y+x[:,0,:].reshape((-1,1,17))+x[:,1,:].reshape(-1,1,17))) # add new channels
         y = F.relu(self.conv22(y))
-        y = F.relu(self.conv23(y)) 
-        
+        y = F.relu(self.conv23(y))
         y = self.bn2(y)
         
-        y = F.relu(self.conv31(y+x))
+        y = F.relu(self.conv31(y+x[:,0,:].reshape((-1,1,17))+x[:,1,:].reshape(-1,1,17)))
         y = F.relu(self.conv32(y))
         y = F.relu(self.conv33(y))
         
         y = self.bn3(y)
         
-        y = F.relu(self.conv41(y+x))
+        y = F.relu(self.conv41(y+x[:,0,:].reshape((-1,1,17))+x[:,1,:].reshape(-1,1,17)))
         y = F.relu(self.conv42(y))
         y = F.relu(self.conv43(y))
         
         return y
+    
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import json
 
 class Points(Dataset):
-    def __init__(self, path_to_json, kpts_size=17, train=False, val=False, prob = 1./17, conv=False):
+    def __init__(self, path_to_json, kpts_size=17, train=False, val=False, conv=False):
         with open(path_to_json) as f:
             self.files = json.load(f)
         if val:
@@ -22,49 +22,35 @@ class Points(Dataset):
         self.conv = conv
         self.train = train
         self.kpts_size = kpts_size
-        self.prob = prob
     def __len__(self):
         return len(self.files)
     def normalize(self, keypoints):
         kpts_x, kpts_y = np.array([keypoints[0::3]]), np.array([keypoints[1::3]])
         kpts_x = kpts_x - np.min(kpts_x)
         kpts_y = kpts_y - np.min(kpts_y)
-        kpts_x = kpts_x / np.max(kpts_x) + 0.001
-        kpts_y = kpts_y / np.max(kpts_y) + 0.001
+        kpts_x = kpts_x / np.max(kpts_x)
+        kpts_y = kpts_y / np.max(kpts_y)
         return np.array([kpts_x, kpts_y])
-    def get_elim_mask(self):
-        #mask = torch.rand(1,self.kpts_size)
-        number = torch.randint(0, self.kpts_size, (1,)).item()
-        mask = 1. * torch.tensor([(i!=number) for i in range(self.kpts_size)])
-        #mask = torch.cat([mask.t(),mask.t()], dim=0).reshape(-1)
-        return mask, number
-    def set_elim_prob(self, new_prob):
-        self.prob = new_prob
+    
     def __getitem__(self, idx):
-        kpts_original = torch.tensor(self.normalize(self.files[idx]['keypoints']), 
-                                     dtype=torch.float32)
-        get_elim_mask, idx = self.get_elim_mask()
-        #kpts_norm = kpts_original * get_elim_mask
-        kpts_original_ = torch.zeros((2 * self.kpts_size), dtype=torch.float32)
-        kpts_norm = torch.zeros_like(kpts_original_)
-        kpts_original_[0::2] = kpts_original[0][0]
-        kpts_original_[1::2] = kpts_original[1][0]
-        kpts_norm[::2] = kpts_original_[0::2]*get_elim_mask
-        kpts_norm[1::2] = kpts_original_[1::2]*get_elim_mask
-        if self.conv:
-            kpts_norm = kpts_norm.reshape(1, 2*self.kpts_size)
-            kpts_original_ = kpts_original_.reshape(1, 2*self.kpts_size)
-        if self.train:
-            return kpts_norm, kpts_original_, idx
-        return kpts_norm, kpts_original_, idx
+        kpts_original = self.normalize(self.files[idx]['keypoints'])
+        
+        kpts_out = np.array([kpts_original[0][0],
+                                kpts_original[1][0]], dtype=np.float32)
+        return torch.tensor(kpts_out), torch.tensor(kpts_out.copy())
+
+def get_elim_mask(val_thresh=0.6):
+    mask = 1.*(torch.rand(x.shape[0], 1, x.shape[-1]) > val_thresh*torch.rand(size=(1,)).item())
+    mask = torch.cat([mask, mask], dim=1)
+    return mask
 
 if __name__ == "__main__":
 
     cuda = False
-    dset_train = Points("kpts_valid.json", train=True, prob=1./14, conv=True)
+    dset_train = Points("kpts_valid.json", train=True, conv=True)
     dset_val = Points("kpts_valid.json", val=True, conv=True)
 
-    model = PointSolver_conv(torch.tensor(np.load("very_important_constant.npy")))
+    model = PointSolver_conv()
     if cuda:
         model.cuda()
         model.constant = model.constant.cuda()
@@ -76,12 +62,17 @@ if __name__ == "__main__":
     train_loader = DataLoader(dset_train, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
     val_loader = DataLoader(dset_val, batch_size=BATCH_SIZE, drop_last=True)
 
+    print("Starting train....\n")
+
     for i in range(N_EPOCHS):
+
         loss_epoch = 0.
         joint_error_per_epoch = []
-    
+
         model.train()
-        for x, y, idx in train_loader:
+        for x, y in train_loader:
+            mask = get_elim_mask()
+            x = x*mask
             if cuda:
                 x = x.cuda()
                 y = y.cuda()
@@ -91,47 +82,49 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
             loss_epoch += loss.item()
-            y_pred = y_pred.reshape(-1, 34)
-            y = y.reshape(-1,34)
-            joint_error = torch.tensor([y_pred[i][2*idx[i]] - y[i][2*idx[i]] \
-                                    for i in range(BATCH_SIZE)]).abs().mean().item()
-        
-            joint_error_per_epoch.append(joint_error)
-    
-        print(f"Epoch {i+1}, joint error {np.array(joint_error_per_epoch).mean()}")
+            joint_error = (torch.abs(y_pred - y)*mask).sum(dim=1).reshape(-1)
+            joint_error = joint_error[joint_error != 0.]
+            #print("type:",type(joint_error))
+
+            joint_error_per_epoch.append(joint_error.mean().item())
+
+        print("Epoch %d \t train joint error %.5f" %(i+1, np.array(joint_error_per_epoch).mean()))
         with torch.no_grad():
             model.eval()
             val_joint_error_per_epoch = []
             val_joints_5_per_epoch = []
             val_joints_10_per_epoch = []
             val_joints_125_per_epoch = []
-            for x, y, idx in val_loader:
+            for x, y in val_loader:
+                mask = get_elim_mask()
+                x = x*mask
                 if cuda:
                     x = x.cuda()
                     y = y.cuda()
                 y_pred = model(x)
                 loss = criterion(y_pred, y)
-                
-                y_pred = y_pred.reshape(-1, 34)
-                y = y.reshape(-1,34)
                 loss_epoch += loss.item()
-                joint_error = torch.tensor([y_pred[i][2*idx[i]] - y[i][2*idx[i]] \
-                                            for i in range(BATCH_SIZE)]).abs().mean().item()
-                correct_joints_share_5 = ((torch.tensor([y_pred[i][2*idx[i]] - y[i][2*idx[i]] \
-                                            for i in range(BATCH_SIZE)]).abs()) < 0.05).sum().item()*1./BATCH_SIZE
-                correct_joints_share_10 = ((torch.tensor([y_pred[i][2*idx[i]] - y[i][2*idx[i]] \
-                                            for i in range(BATCH_SIZE)]).abs()) < 0.1).sum().item()*1./BATCH_SIZE
-                correct_joints_share_125 = ((torch.tensor([y_pred[i][2*idx[i]] - y[i][2*idx[i]] \
-                                            for i in range(BATCH_SIZE)]).abs()) < 0.125).sum().item()*1./BATCH_SIZE
+                joint_error = (torch.abs(y_pred - y)*mask).sum(dim=1).reshape(-1)
+                shape_first = len(joint_error)
+                joint_error = joint_error[joint_error != 0.]
+                #print(len(joint_error), shape_first)
+                #break
+                correct_joints_share_5 = (joint_error < 0.05).sum().item()/len(joint_error)
+                #print("len y is", len(y))
+                correct_joints_share_10 = (joint_error < 0.1).sum().item()/len(joint_error)
+                correct_joints_share_125 = (joint_error < 0.125).sum().item()/len(joint_error)
                 val_joints_5_per_epoch.append(correct_joints_share_5)
                 val_joints_10_per_epoch.append(correct_joints_share_10)
                 val_joints_125_per_epoch.append(correct_joints_share_125)
-                val_joint_error_per_epoch.append(joint_error)
+                val_joint_error_per_epoch.append(joint_error.mean().item())
+            #print(val_joint_error_per_epoch)
             val_joint_error_per_epoch = np.array(val_joint_error_per_epoch).mean()
             val_joints_5_per_epoch = np.array(val_joints_5_per_epoch).mean()
             val_joints_10_per_epoch = np.array(val_joints_10_per_epoch).mean()
             val_joints_125_per_epoch = np.array(val_joints_125_per_epoch).mean()
-            print(f"val_joint_error {val_joint_error_per_epoch},\n\
-            jts_5: {val_joints_5_per_epoch}, jts_10: {val_joints_10_per_epoch},\
-                jts_125: {val_joints_125_per_epoch}")
-        torch.save(model.state_dict(), f"conv_jts5_{val_joints_5_per_epoch}_jts_125_{val_joints_125_per_epoch}_jts_error_{val_joint_error_per_epoch}.pt")
+            print("val_joint_error %.2f,\njts_5: %.2f\tjts_10: %.2f\tjts_125: %.2f"%(val_joint_error_per_epoch, 
+                                                                                    val_joints_5_per_epoch, 
+                                                                                    val_joints_10_per_epoch, 
+                                                                                    val_joints_125_per_epoch))
+        model_name = "conv_missed_jts5_%.2f_jts_125_%.2f_jts_error_%.2f.pt"%(val_joints_5_per_epoch, val_joints_125_per_epoch, val_joint_error_per_epoch)
+        torch.save(model.state_dict(), model_name)
